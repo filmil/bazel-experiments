bca2ceed2f84b6c154a3bcd08c712b81e5fdf865
Filip Filmar <filmil@gmail.com>
2025-12-24 19:43:27 -0800

    fix: this links

--- llvm/toolchain/bin/cc_wrapper.sh
+++ llvm/toolchain/bin/cc_wrapper.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
+./external/toolchains_llvm++llvm+llvm_toolchain/bin/cc_wrapper.sh "${@}"
 
-ls ./external/toolchains_llvm++llvm+llvm_toolchain_llvm
-
-./external/toolchains_llvm++llvm+llvm_toolchain/bin/cc_wrapper.sh "${0}"
