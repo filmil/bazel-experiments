0b80d7e28be62ccfc5d18ac503dce02e1582cfc4
Filip Filmar <filmil@gmail.com>
2025-12-24 20:31:51 -0800

    fix: update the builder and libs so that things seem to pass

--- llvm/toolchain/bin/forwarder.bash
+++ llvm/toolchain/bin/forwarder.bash
@@ -3,7 +3,6 @@
 readonly _scriptname="$(basename ${0})"
 echo "${_scriptname}"
 
-"external/toolchains_llvm++llvm+llvm_toolchain/${_scriptname}" \
-    "${@}"
+"external/toolchains_llvm++llvm+llvm_toolchain_llvm/bin/${_scriptname}" "${@}"
 
 # vim: ft=bash :
