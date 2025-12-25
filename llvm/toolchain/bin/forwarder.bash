23bb42c333798d12b4fd92c4308b690d7eb2fe15
Filip Filmar <filmil@gmail.com>
2025-12-24 21:22:03 -0800

    fix: allow building startup code as a true library

--- llvm/toolchain/bin/forwarder.bash
+++ llvm/toolchain/bin/forwarder.bash
@@ -1,8 +1,6 @@
 #! /usr/bin/env bash
 
 readonly _scriptname="$(basename ${0})"
-echo "${_scriptname}"
-
 "external/toolchains_llvm++llvm+llvm_toolchain_llvm/bin/${_scriptname}" "${@}"
 
 # vim: ft=bash :
